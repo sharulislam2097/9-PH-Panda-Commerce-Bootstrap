0697b3d8f768809d8b754152e3dcee81a6e57939
sharulislam <sharulislam60@gmail.com>
2023-02-28 01:22:28 +0600

    js practice

--- js/style.js
+++ js/style.js
@@ -1,10 +1,38 @@
-const h2title =document.getElementsByTagName('h2');
-for( h2 of h2title){
-    h2.style.color='chocolate';
+//2
+const h2title = document.getElementsByTagName("h2");
+for (h2 of h2title) {
+  h2.style.color = "lightblue";
+}
+//3
+const bagsection = document.getElementById("backpack");
+bagsection.style.backgroundColor = "tomato";
+//4
+cardsection = document.getElementsByClassName("card");
+for (card of cardsection) {
+  card.style.borderRadius = "30px";
+}
+//5
+function test() {
+  console.log("button click test");
 }
 
+//6
+document
+  .getElementById("exampleInputEmail1")
+  .addEventListener("keyup", function (event) {
+    const text = event.target.value;
 
+    const submitBtn = document.getElementById("btn-submit");
+    if (text === "email") {
+      submitBtn.removeAttribute("disabled");
+    } else {
+      submitBtn.setAttribute("disabled", true);
+    }
+  });
 
-
-
-
+  //9
+  const staysection=document.getElementById('stayfooter');
+  staysection.addEventListener('dblclick',function(){
+    
+    staysection.style.backgroundColor='powderblue';
+  })
